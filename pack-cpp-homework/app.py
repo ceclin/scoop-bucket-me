064a6b1d19a8c99c1617aced986c7fd53809e6a8
Cecil Lin <icecillin@outlook.com>
2019-10-05 03:10:45 +0800

    [pack-cpp-homework] add command option: --homework-version

--- pack-cpp-homework/app.py
+++ pack-cpp-homework/app.py
@@ -10,10 +10,14 @@ parser.add_argument('--name', '-n', dest='name',
                     default=os.environ.get('CPP_HOMEWORK_NAME', 'unknown'))
 parser.add_argument('--student-number', '-sn', dest='student_number',
                     default=os.environ.get('CPP_HOMEWORK_STUDENT_NUMBER', 'unknown'))
+parser.add_argument('--homework-version', '-hv',
+                    dest='homework_version', type=int, default=1)
 args = parser.parse_args()
 
 name = args.name
 student_number = args.student_number
+homework_version = args.homework_version
+
 
 def pack_dir(dir_path):
     base_dir = Path(dir_path).resolve()
@@ -23,7 +27,12 @@ def pack_dir(dir_path):
     if len(target_dirs) == 0:
         print('no target found')
         return
-    output_file = base_dir / f'{student_number}_{name}_hw{base_dir.name}.zip'
+    if homework_version > 0:
+        output_file = base_dir / \
+            f'{student_number}_{name}_hw{base_dir.name}_v{homework_version}.zip'
+    else:
+        output_file = base_dir / \
+            f'{student_number}_{name}_hw{base_dir.name}.zip'
     print(f'writing {output_file}')
     with zipfile.ZipFile(output_file, 'w') as output:
         for dir in target_dirs:
